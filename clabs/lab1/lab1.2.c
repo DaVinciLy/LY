2937c75d1c54f8eb6d553d0dfebcb98bab6be34c
DaVinciLy <1419220793@qq.com>
2019-11-14 18:48:10 -0800

    lab1 2

---
+++ clabs/lab1/lab1.2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+    int n;
+    int i=0;
+    int a[100];
+    int count=0;
+    printf(":\n")
+    scanf("%d",&n);
+    printf(":")
+    while(n){
+        a[i]=n%2
+        i++;
+        n/=2;
+    }
+    for(int j=i-1;j>=0;j--){
+        printf("%d",a[j]);
+        if(a[j]==1){
+            count++;
+        }
+    }
+    printf("\n\"1\"%d\n",count);
+    system("pause")
+    return 0;
+}
